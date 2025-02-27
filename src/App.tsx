89658e5021c7c97aba41ea7223a77ce178427449
klae32 <techie.r13@gmail.com>
2025-02-26 22:09:55 -0700

    Created and added NavBar to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import { Hide } from "@chakra-ui/media-query";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -12,8 +13,8 @@ function App() {
           lg: `"nav nav" "aside main"`,
         }}
       >
-        <GridItem area="nav" bg="coral">
-          Nav
+        <GridItem area="nav">
+          <NavBar />
         </GridItem>
         <Hide breakpoint="(max-width: 1024px)">
           <GridItem area="aside" bg="gold">
